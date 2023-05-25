96cf9c6063cf7c0db32752204e6fe77a299b9009
eda <e.karaosmanoglu@tudelft.nl>
2023-05-25 14:22:51 +0200

    docstring for source code documentation

--- convert_temperature.py
+++ convert_temperature.py
@@ -1,3 +1,13 @@
 def fahrenheit_to_celcius(temperature_fahrenheit):
+	"""
+	Converts temperature in fahrenheit to celcius
+
+	Parameters:
+	temperature_fahrenheit
+
+	Returns
+	temperature _celcius
+
+	"""
 	temperature_celcius = (temperature_fahrenheit - 32)* (5.0/9.0)
 	return temperature_celcius
